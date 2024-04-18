877aa0a94c9fa96bc753167f83ee17f37c4674d6
szabinakov <szabina@charisma.ai>
2024-04-18 17:22:37 +0100

    tidied easypeasy

--- test/test-app/src/withEasyPeasy.tsx
+++ test/test-app/src/withEasyPeasy.tsx
@@ -1,33 +1,13 @@
-// import { NextPage } from "next";
-import { StoreProvider, Store } from "easy-peasy";
+import { StoreProvider } from "easy-peasy";
 import hoistNonReactStatics from "hoist-non-react-statics";
 
 import { initStore } from "./lib/store";
-import { StoreModel } from "./lib/model";
+import { ComponentType } from "react";
 
-declare global {
-  interface Window {
-    __NEXT_REDUX_STORE__: Store<StoreModel>;
-  }
-}
+const store = initStore();
 
-function getOrCreateStore() {
-  // Always make a new store if server, otherwise state is shared between requests
-  if (typeof window === "undefined") {
-    return initStore();
-  }
-
-  // Create store if unavailable on the client and set it on the window object
-  /* eslint-disable no-underscore-dangle */
-  if (!window.__NEXT_REDUX_STORE__) {
-    window.__NEXT_REDUX_STORE__ = initStore();
-  }
-  return window.__NEXT_REDUX_STORE__;
-}
-
-const withEasyPeasy = (PageComponent) => {
-  const WithEasyPeasy = (pageProps) => {
-    const store = getOrCreateStore();
+const withEasyPeasy = (PageComponent: ComponentType<any>) => {
+  const WithEasyPeasy = (pageProps: any) => {
     return (
       <StoreProvider store={store}>
         <PageComponent {...pageProps} />
