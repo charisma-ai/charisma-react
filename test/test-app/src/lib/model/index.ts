2e8b860d384750030cd6e55f4e0fca19440a18a0
John James <19880152+John-A-J@users.noreply.github.com>
2024-04-19 11:41:58 +0100

    feat[#34]: remove unused state management of chats

--- test/test-app/src/lib/model/index.ts
+++ test/test-app/src/lib/model/index.ts
@@ -1,14 +1,11 @@
 import chat, { ChatModel } from "./chat";
-import ui, { UIModel } from "./ui";
 
 export interface StoreModel {
   chat: ChatModel;
-  ui: UIModel;
 }
 
 const model: StoreModel = {
   chat,
-  ui,
 };
 
 export default model;
